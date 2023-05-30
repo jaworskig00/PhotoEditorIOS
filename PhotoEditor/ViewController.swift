513a9a67890d6e648e5d75f304886114a3b5dae8
Grzegorz Jaworski <g.jaworski@selleo.com>
2023-05-30 18:45:58 +0200

    Initial Commit

---
+++ PhotoEditor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoEditor
+//
+//  Created by Grzegorz Jaworski on 30/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
